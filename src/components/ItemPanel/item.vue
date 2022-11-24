16cb54dda11c4d6ed0247f0ef0e7ccf347851c28
jiaozh1 <jiaozh1@lenovo.com>
2022-11-24 09:15:31 +0800

    first commit

--- src/components/ItemPanel/item.vue
+++ src/components/ItemPanel/item.vue
@@ -23,7 +23,9 @@
             
             >
               <span class="pannel-type-icon" :style="{'background-image':'url('+item.backImage+')'}"></span>
-              <div style="text-align: center; ">{{item.node_type}}</div>
+              <div style="text-align: center; overflow: hidden;
+white-space: nowrap;
+text-overflow: ellipsis;">{{item.node_type}}</div>
             </div>
           </el-col>
       </el-row>
@@ -42,6 +44,37 @@ import agentserverSvg from "@/assets/icons/agentserver-green.svg";
 import appserverSvg from "@/assets/icons/appserver-green.svg";
 import clientSvg from "@/assets/icons/client-green.svg";
 
+import cloudSvg from "@/assets/icons/cloud-green.svg";
+import dataSvg from "@/assets/icons/data-green.svg";
+import databaseSvg from "@/assets/icons/database-green.svg";
+import dirSvg from "@/assets/icons/dir-green.svg";
+import dnsSvg from "@/assets/icons/dns-green.svg";
+import firewallSvg from "@/assets/icons/firewall-green.svg";
+import hubSvg from "@/assets/icons/hub-green.svg";
+import loadbalanceSvg from "@/assets/icons/loadbalance-green.svg";
+import mailserverSvg from "@/assets/icons/mailserver-green.svg";
+import pingSvg from "@/assets/icons/ping-green.svg";
+import routerSvg from "@/assets/icons/router-green.svg";
+import routerHubSvg from "@/assets/icons/router-hub-green.svg";
+import routerwallblueSvg from "@/assets/icons/routerwallblue-green.svg";
+import serverSvg from "@/assets/icons/server-green.svg";
+
+import storageSvg from "@/assets/icons/storage-green.svg";
+import sublogicSvg from "@/assets/icons/sublogic-green.svg";
+
+import switch1Svg from "@/assets/icons/switch1-green.svg";
+import switch2Svg from "@/assets/icons/switch2-green.svg";
+import switch3Svg from "@/assets/icons/switch3-green.svg";
+import switch7blueSvg from "@/assets/icons/switch7blue-green.svg";
+import urlSvg from "@/assets/icons/url-green.svg";
+
+import urlblueSvg from "@/assets/icons/urlblue-green.svg";
+import vcenterSvg from "@/assets/icons/vcenter-green.svg";
+import vesselSvg from "@/assets/icons/vessel-green.svg";
+import vpnSvg from "@/assets/icons/vpn-green.svg";
+import webserverSvg from "@/assets/icons/webserver-green.svg";
+
+
 export default {
   data() {
     return {
@@ -51,9 +84,9 @@ export default {
       offsetY: 0,
       list: [
         {
-          name: "数据中心",
+          name: "datacenter",
           node_type: 'datacenter',
-          label: "数据中心",
+          label: "datacenter",
           width: 40,
           height: 40,
           type: "node",
@@ -70,8 +103,8 @@ export default {
         },
 
         {
-          name: "城市",
-          label: "城市",
+          name: "componany",
+          label: "componany",
           node_type: 'componany',
           width: 40,
           height: 40,
@@ -107,8 +140,8 @@ export default {
           isDoingEnd: true
         },
         {
-          name: "agent服务器",
-          label: "服务器",
+          name: "agent",
+          label: "agent",
           node_type: 'agent',
           width: 40,
           height: 40,
@@ -125,8 +158,8 @@ export default {
           isDoingEnd: true
         },
         {
-          name: "app服务器",
-          label: "app服务器",
+          name: "app server",
+          label: "app server",
           node_type: 'app',
           width: 40,
           height: 40,
@@ -143,8 +176,8 @@ export default {
           isDoingStart: true,
         },
         {
-          name: "客户端",
-          label: "客户端",
+          name: "client",
+          label: "client",
           node_type: 'client',
           width: 40,
           height: 40,
@@ -159,7 +192,478 @@ export default {
           inPoints: [[0, 0.5]],
           outPoints: [[1, 0.5]],
           isDoingEnd: true
-        }
+        },
+        {
+          name: "cloud",
+          label: "cloud",
+          node_type: 'cloud',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          cloudSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+
+        {
+          name: "data",
+          label: "data",
+          node_type: 'data',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          dataSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "database",
+          label: "database",
+          node_type: 'database',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          databaseSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "dir",
+          label: "dir",
+          node_type: 'dir',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          dirSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "dns",
+          label: "dns",
+          node_type: 'dns',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          dnsSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "firewall",
+          label: "firewall",
+          node_type: 'firewall',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          firewallSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "hub",
+          label: "hub",
+          node_type: 'hub',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          hubSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "loadbalance",
+          label: "loadbalance",
+          node_type: 'loadbalance',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          loadbalanceSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "mailserver",
+          label: "mailserver",
+          node_type: 'mailserver',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          mailserverSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "ping",
+          label: "ping",
+          node_type: 'ping',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          pingSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "router",
+          label: "router",
+          node_type: 'router',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          routerSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "routerwallblue",
+          label: "routerwallblue",
+          node_type: 'routerwallblue',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          routerwallblueSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "router-hub",
+          label: "router-hub",
+          node_type: 'routerHub',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          routerHubSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "server",
+          label: "server",
+          node_type: 'server',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          serverSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "storage",
+          label: "storage",
+          node_type: 'storage',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          storageSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "sublogic",
+          label: "sublogic",
+          node_type: 'sublogic',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          sublogicSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+
+        
+        {
+          name: "switch1",
+          label: "switch1",
+          node_type: 'switch1',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          switch1Svg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "switch2",
+          label: "switch2",
+          node_type: 'switch2',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          switch2Svg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "switch3",
+          label: "switch3",
+          node_type: 'switch3',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          switch3Svg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "switch7blue",
+          label: "switch7blue",
+          node_type: 'switch7blue',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          switch7blueSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "url",
+          label: "url",
+          node_type: 'url',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          urlSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "urlblue",
+          label: "urlblue",
+          node_type: 'urlblue',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          urlblueSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "vcenter",
+          label: "vcenter",
+          node_type: 'vcenter',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          vcenterSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "vessel",
+          label: "vessel",
+          node_type: 'vessel',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          vesselSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "vpn",
+          label: "vpn",
+          node_type: 'vpn',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          vpnSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
+        {
+          name: "webserver",
+          label: "webserver",
+          node_type: 'webserver',
+          width: 40,
+          height: 40,
+          type: "node",
+          x: 0,
+          y: 0,
+          shape: "customNode",
+          color: "#1890ff",
+          backImage:
+          webserverSvg,
+          stateImage: okSvg,
+          inPoints: [[0, 0.5]],
+          outPoints: [[1, 0.5]],
+          isDoingEnd: true
+        },
       ]
     };
   },
